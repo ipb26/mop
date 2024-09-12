672a3cb9dc4e76fe0583401ec11925cc5a3bbd17
Sam Barrow <sam@sambarrow.com>
2024-09-12 15:54:39 -0400

    .

--- src/base.ts
+++ src/base.ts
@@ -38,7 +38,7 @@ export type ErrorFactory<T> = ValueOrFactory<ErrorInput | readonly ErrorFactory<
 /**
  * An error for a factory. Gets combined with a value and turned into a MapError.
  */
-export type ErrorInfo = Pick<MapError, "path" | "message" | "value">
+export type ErrorInfo = Pick<MapError, "path" | "message">
 
 /**
  * Generates an error using a factory and a value.
