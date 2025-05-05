1448f7a1a9bf8f1900ee3ef54f77135dda2a3b0f
Sam Barrow <sam@sambarrow.com>
2025-05-05 00:41:54 +0000

    .

--- src/specialty.ts
+++ src/specialty.ts
@@ -13,7 +13,7 @@ export const localStorage = flow(noOp<string>, map(key => window.localStorage.ge
  * Creates a mapper that requires that two keys of an object match.
  * The error can be assigned to the path of the first key, the second key, or both.
  */
-export const confirm = <I>(a: keyof I & string, b: keyof I & string, applyTo: "initial" | "confirm" | "both" = "confirm", error: ErrorFactory<I> = "Confirmation does not match.") => {
+export const confirm = <I>(a: keyof I & string, b: keyof I & string, applyTo: "initial" | "confirm" | "both" = "confirm", error: ErrorFactory<I> = "Confirmation does not match") => {
     return predicate<I>(input => input[a] === input[b], input => {
         const error1 = buildError(error, input).map(error => ({ ...error, path: [a], value: input[a] }))
         const error2 = buildError(error, input).map(error => ({ ...error, path: [b], value: input[b] }))
